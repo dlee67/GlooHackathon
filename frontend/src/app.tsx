2be1b8427e70d82ca85bb9e84c26dfbeb1484a6f
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-09-14 03:12:57 -0600

    center on gloo in boulder

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -23,7 +23,7 @@ const App = () => (
     <APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
         <Map
             defaultZoom={13}
-            defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
+            defaultCenter={ { lat: 40.0173051, lng: -105.2843275 } }
             onCameraChanged={ (ev: MapCameraChangedEvent) =>
                 console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
             }>
